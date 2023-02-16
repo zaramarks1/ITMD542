138850185fb2fe83c6d884df4b3e68b22db217a6
Zara Marks <zara.marks@reseau.eseo.fr>
2023-02-15 19:14:03 -0600

    Added edit page

--- lab2/routes/contact.js
+++ lab2/routes/contact.js
@@ -2,13 +2,13 @@
 var express = require('express');
 var router = express.Router();
 
-const contactsRepo = require('../src/contactsFileRepo');
+const contactRepo = require('../src/contactsFileRepo');
 
 const Contact = require('../src/Contact')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const data = contactsRepo.findAll();
+  const data = contactRepo.findAll();
   res.render('contacts', { title: 'contacts', contacts : data });
 });
 
@@ -25,6 +25,32 @@ router.post('/add', function(req, res, next) {
     contactsRepo.create(newContact);
     res.redirect('/contact');
 });
+
+/* GET single contact. */
+router.get('/:uuid', function(req, res, next) {
+  const data = contactRepo.findById(req.params.uuid);
+  res.render('contact', { title: 'contact', contact : data });
+});
+
+/* GET edit page  */
+router.get('/:uuid/edit', function(req, res, next){
+  const contact = contactRepo.findById(req.params.uuid);
+  res.render('contact_edit', { title: 'contact', contact : contact });
+});
+
+/* POST edit Contact */
+router.post('/:uuid/edit', function(req, res, next) {
+
+  //const contact = contactRepo.findById(req.params.uuid);
+
+  const newContact = new Contact(req.params.uuid, req.body.firstName, req.body.lastName, req.body.email, req.body.notes, ''); 
+
+  contactRepo.update(newContact);
+  res.redirect('/contact');
+});
+
+/* Get delete contact page */
+
 
 
 module.exports = router;
