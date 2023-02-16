8ced01087a2efe31317bd50d5dc0bb3945a3f9e3
Zara Marks <zara.marks@reseau.eseo.fr>
2023-02-15 22:41:34 -0600

    css added

--- lab2/controller/contactsController.js
+++ lab2/controller/contactsController.js
@@ -5,7 +5,7 @@ const { validationResult } = require('express-validator');
 /* GET contacts . */
 exports.contacts_list = function(req, res, next) {
     const data = contactsRepo.findAll();
-    res.render('contacts', { title: 'contacts', contacts : data });
+    res.render('contacts', { title: 'Contact List Page', contacts : data });
 };
 
 exports.contacts_add_get = function(req, res, next){
@@ -25,18 +25,18 @@ exports.contacts_add_post = function(req, res, next){
 
 exports.contacts_single = function(req, res, next){
     const data = contactsRepo.findById(req.params.uuid);
-    res.render('contact', { title: 'contact', contact : data });
+    res.render('contact', { title: 'Contact Information', contact : data });
 };
 
 exports.contacts_edit_get = function(req, res, next){
     const contact = contactsRepo.findById(req.params.uuid);
-    res.render('contact_edit', { title: 'contact', contact : contact });
+    res.render('contact_edit', { title: 'Edit Contact Information', contact : contact });
 };
 
 exports.contacts_edit_post = function(req, res, next){
     const result = validationResult(req);
     if (!result.isEmpty()) {
-      res.render('contact_edit', { title: 'Edit a contact', msg: result.array() });
+      res.render('contact_edit', { title: 'Edit a Contact', msg: result.array() });
     }else{
     const newContact = new Contact(req.params.uuid, req.body.firstName, req.body.lastName, req.body.email, req.body.notes, ''); 
     contactsRepo.update(newContact);
@@ -46,7 +46,7 @@ exports.contacts_edit_post = function(req, res, next){
 
 exports.contacts_delete_get = function(req, res, next){
     const contact = contactsRepo.findById(req.params.uuid);
-    res.render('contact_delete', { title: 'contact', contact : contact });
+    res.render('contact_delete', { title: 'Are you sure you want to delete the following Contact?', contact : contact });
 };
 
 exports.contacts_delete_post = function(req, res, next){
