27fc2d359cb8e854386abe274cc7fde577e37878
Zara Marks <zara.marks@reseau.eseo.fr>
2023-03-28 15:02:29 -0500

    Lab 4 : added contactsMongoRepo

---
+++ lab4/src/contactsMongoRepo.js
@@ -0,0 +1,81 @@
+const { MongoClient, ObjectId } = require('mongodb');
+const Contact = require('./Contact');
+
+//const url = 'mongodb://localhost:27017';
+const url = process.env.MONGODB_URL;
+const client = new MongoClient(url);
+
+async function run() {
+  await client.connect();
+  return 'Connected to the MongoDB server...';
+}
+
+run()
+  .then(console.log)
+  .catch(console.error);
+
+const repo = {
+  findAll: async () => {
+    let contacts = [];
+    const contactsColl = client.db('zarademo').collection('contacts');
+    const cursor = contactsColl.find({});
+    await cursor.forEach(doc => {
+      const contact = new Contact(doc._id.toString(), doc.firstName, doc.lastName, doc.email, doc.notes,new Date(doc.date).toString());
+      contacts.push(contact);
+    });
+    return contacts;
+  },
+  findById: async (uuid) => {
+    const contactsColl = client.db('zarademo').collection('contacts');
+    const filter = {
+      '_id': new ObjectId(uuid)
+    };
+    const doc = await contactsColl.findOne(filter);
+    return new Todo(doc._id.toString(), doc.text);
+  },
+
+  create: async (contact) => {
+    const doc = {firstName : contact.firstName,
+        lastName: contact.lastName, 
+        email : contact.email, 
+        notes: contact.notes,
+        date: new Date(contact.date).toString()
+    };
+
+    const contactsColl = client.db('zarademo').collection('contacts');
+    const result = await contactsColl.insertOne(doc);
+    console.log(`A document was inserted with the _id: ${result.insertedId}`);
+  },
+
+  deleteById: async (uuid) => {
+    const contactsColl = client.db('zarademo').collection('contacts');
+    const filter = {
+      '_id': new ObjectId(uuid)
+    };
+    const result = await contactsColl.deleteOne(filter);
+    if (result.deletedCount === 1) {
+      console.log("Successfully deleted a documents.");
+    } else {
+      console.log("No documents matched the query. Deleted 0 documents.");
+    }
+  },
+  update: async (contact) => { 
+    const contactsColl = client.db('zarademo').collection('contacts');
+    const filter = {
+      '_id': new ObjectId(todo.id)
+    };
+    const updateDoc = {
+      $set: {
+        firstName : contact.firstName,
+        lastName: contact.lastName, 
+        email : contact.email, 
+        notes: contact.notes,
+        date : contact.date
+      }
+    };
+    const result = await contactsColl.updateOne(filter, updateDoc);
+    console.log(`${result.matchedCount} docs matched the filter, updated ${result.modifiedCount} document(s)`);
+  },
+};
+
+module.exports = repo;
