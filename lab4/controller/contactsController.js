33ee6a9a14da29436ceb3bdbb85ea1bb024fa54c
Zara Marks <zara.marks@reseau.eseo.fr>
2023-03-28 15:21:21 -0500

    Lab 4: Change controller

--- lab4/controller/contactsController.js
+++ lab4/controller/contactsController.js
@@ -1,11 +1,10 @@
-const contactsRepo = require('../src/contactsFileRepo');
-const contactsSQLRepo = require('../src/contactsSQLiteRepo');
+const contactsRepo = require('../src/contactsMongoRepo');
 const Contact = require('../src/Contact')
 const { validationResult } = require('express-validator');
 
 /* GET contacts . */
 exports.contacts_list = function(req, res, next) {
-    const data = contactsSQLRepo.findAll();
+    const data = contactsRepo.findAll();
     res.render('contacts', { title: 'Contact List Page', contacts : data });
 };
 
@@ -19,18 +18,18 @@ exports.contacts_add_post = function(req, res, next){
       res.render('contact_add', { title: 'Add a contact', msg: result.array() });
     }else{
       const newContact = new Contact('', req.body.firstName, req.body.lastName, req.body.email, req.body.notes, ''); 
-      contactsSQLRepo.create(newContact);
+      contactsRepo.create(newContact);
       res.redirect('/contacts');
     }
 };
 
 exports.contacts_single = function(req, res, next){
-    const data = contactsSQLRepo.findById(req.params.uuid);
+    const data = contactsRepo.findById(req.params.uuid);
     res.render('contact', { title: 'Contact Information', contact : data });
 };
 
 exports.contacts_edit_get = function(req, res, next){
-    const contact = contactsSQLRepo.findById(req.params.uuid);
+    const contact = contactsRepo.findById(req.params.uuid);
     res.render('contact_edit', { title: 'Edit Contact Information', contact : contact });
 };
 
@@ -40,17 +39,17 @@ exports.contacts_edit_post = function(req, res, next){
       res.render('contact_edit', { title: 'Edit a Contact', msg: result.array() });
     }else{
     const newContact = new Contact(req.params.uuid, req.body.firstName, req.body.lastName, req.body.email, req.body.notes, ''); 
-    contactsSQLRepo.update(newContact);
+    contactsRepo.update(newContact);
     res.redirect('/contacts');
     }
 };
 
 exports.contacts_delete_get = function(req, res, next){
-    const contact = contactsSQLRepo.findById(req.params.uuid);
+    const contact = contactsRepo.findById(req.params.uuid);
     res.render('contact_delete', { title: 'Are you sure you want to delete the following Contact?', contact : contact });
 };
 
 exports.contacts_delete_post = function(req, res, next){
-    contactsSQLRepo.deleteById(req.params.uuid);
+    contactsRepo.deleteById(req.params.uuid);
     res.redirect('/contacts');
 };
